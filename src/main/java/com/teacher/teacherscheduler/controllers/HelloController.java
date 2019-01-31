1b042d6de391bd80c8ec5e0756ae806706c04754
lindskelsey <lindsaykelsey@gmail.com>
2019-01-30 18:48:08 -0600

    Revert "Created skeleton code for table"
    This reverts commit 28a3cbbcdd1b5337fe95ce90bfdcae75161bba50.

--- src/main/java/com/teacher/teacherscheduler/controllers/HelloController.java
+++ src/main/java/com/teacher/teacherscheduler/controllers/HelloController.java
@@ -1,10 +1,7 @@
 package com.teacher.teacherscheduler.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.xml.ws.RequestWrapper;
@@ -12,15 +9,9 @@ import javax.xml.ws.RequestWrapper;
 @Controller
 public class HelloController {
 
-    @RequestMapping(value = "")
+    @RequestMapping(value="")
     @ResponseBody
-    public String index() {
+    public String index(){
         return "Hello World";
-    }
-//Classroom number, Teacher//
-    @RequestMapping(value = "/view", method = RequestMethod.GET)
-    public String viewPeriod(Model model) {
-
-        return "viewPeriod";
     }
 }
